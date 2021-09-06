ead890123e68734a66bac8e79a05966ba8e9c7eb
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2021-09-06 16:08:28 -0300

    TESTE NAVBAR

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ window.addEventListener(
 //   else
 //   document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-moon-o(?!\S)/g , 'fa fa-sun-o' )
 // })
+// 
+
+
+// 
+// TESTE NAVBAR
+// 
+$(document).ready(function () {
+  var state = false,
+    links = $(".navbar-responsive__link");
+  $("#nav-icon3").click(function () {
+    $(this).toggleClass("open");
+    if (!state) {
+      $(".navbar-responsive").css("transform", "translate3d(0,0,0)");
+      state = true;
+    } else {
+      $(".navbar-responsive").css("transform", "translate3d(-100%,0,0)");
+      state = false;
+    }
+  });
+  $.each(links, function (index, value) {
+    value.addEventListener("click", function () {
+      if (!state) {
+        $(".navbar-responsive").css("transform", "translate3d(0,0,0)");
+        state = true;
+      } else {
+        $(".navbar-responsive").css("transform", "translate3d(-100%,0,0)");
+        state = false;
+      }
+      $("#nav-icon3").removeClass("open");
+    });
+  });
+});
