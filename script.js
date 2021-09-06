580637c2964aef30941f59d5881530fa39fecc2c
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2021-09-06 16:45:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,23 +10,6 @@ window.addEventListener(
   false
 );
 
-// DARK MODE
-// const $html = document.querySelector('html')
-// const $checkbox = document.querySelector('#switch')
-
-// $checkbox.addEventListener('change', function () {
-//   $html.classList.toggle('dark-mode')
-
-//   if ( document.getElementById("mode-icon").className.match(/(?:^|\s)fa fa-sun-o(?!\S)/) ) {
-//   document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-sun-o(?!\S)/g , 'fa fa-moon-o' )
-//   document.getElementById("mode-icon").style = document.getElementById("mode-icon").style.setProperty("padding-left", "5px", "1")
-//   }
-//   else
-//   document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-moon-o(?!\S)/g , 'fa fa-sun-o' )
-// })
-// 
-
-
 // 
 // TESTE NAVBAR
 // 
