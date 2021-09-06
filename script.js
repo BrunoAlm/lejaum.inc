1b1a412c05f7984d1b664b962979ffbe905f661c
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2021-09-06 16:03:50 -0300

    TESTE NAVBAR

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// TESTE DE ANIMAÇÃO COM SCROLL DO FOGUETE
+// ANIMAÇÃO COM SCROLL DO FOGUETE
 window.addEventListener(
   "scroll",
   () => {
@@ -11,16 +11,16 @@ window.addEventListener(
 );
 
 // DARK MODE
-const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#switch')
+// const $html = document.querySelector('html')
+// const $checkbox = document.querySelector('#switch')
 
-$checkbox.addEventListener('change', function () {
-  $html.classList.toggle('dark-mode')
+// $checkbox.addEventListener('change', function () {
+//   $html.classList.toggle('dark-mode')
 
-  if ( document.getElementById("mode-icon").className.match(/(?:^|\s)fa fa-sun-o(?!\S)/) ) {
-  document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-sun-o(?!\S)/g , 'fa fa-moon-o' )
-  document.getElementById("mode-icon").style = document.getElementById("mode-icon").style.setProperty("padding-left", "5px", "1")
-  }
-  else
-  document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-moon-o(?!\S)/g , 'fa fa-sun-o' )
-})
+//   if ( document.getElementById("mode-icon").className.match(/(?:^|\s)fa fa-sun-o(?!\S)/) ) {
+//   document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-sun-o(?!\S)/g , 'fa fa-moon-o' )
+//   document.getElementById("mode-icon").style = document.getElementById("mode-icon").style.setProperty("padding-left", "5px", "1")
+//   }
+//   else
+//   document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-moon-o(?!\S)/g , 'fa fa-sun-o' )
+// })
