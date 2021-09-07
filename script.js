b5521f3fb050622e8b14eddc5ea2b72c5d373a21
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2021-09-07 20:58:26 -0300

    adicionado uma terceira tela e um carrossel

--- script.js
+++ script.js
@@ -19,12 +19,11 @@ const $checkbox = document.querySelector('#switch')
 $checkbox.addEventListener('change', function () {
   $html.classList.toggle('dark-mode')
 
-  if ( document.getElementById("mode-icon").className.match(/(?:^|\s)fa fa-sun-o(?!\S)/) ) {
-  document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-sun-o(?!\S)/g , 'fa fa-moon-o' )
-  document.getElementById("mode-icon").style = document.getElementById("mode-icon").style.setProperty("padding-left", "5px", "1")
-  }
-  else
-  document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace( /(?:^|\s)fa fa-moon-o(?!\S)/g , 'fa fa-sun-o' )
+  if (document.getElementById("mode-icon").className.match(/(?:^|\s)fa fa-sun-o(?!\S)/)) {
+    document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace(/(?:^|\s)fa fa-sun-o(?!\S)/g, 'fa fa-moon-o')
+    document.getElementById("mode-icon").style = document.getElementById("mode-icon").style.setProperty("padding-left", "5px", "1")
+  } else
+    document.getElementById("mode-icon").className = document.getElementById("mode-icon").className.replace(/(?:^|\s)fa fa-moon-o(?!\S)/g, 'fa fa-sun-o')
 })
 
 // 
@@ -56,3 +55,53 @@ $(document).ready(function () {
     });
   });
 });
+
+
+
+// CARROSSEL
+const track = document.querySelector('.track');
+let initialPosition = null;
+let moving = false;
+let transform = 0;
+let lastPageX = 0;
+
+const gestureStart = (e) => {
+  initialPosition = e.pageX;
+  moving = true;
+  const transformMatrix = window.getComputedStyle(track).getPropertyValue('transform');
+  if (transformMatrix !== 'none') {
+    transform = parseInt(transformMatrix.split(',')[4].trim());
+  }
+}
+
+const gestureMove = (e) => {
+  if (moving) {
+    const currentPosition = e.pageX;
+    const diff = currentPosition - initialPosition;
+    if (e.pageX - lastPageX > 0) {
+      if (transform > 0) {
+        return;
+      }
+    }
+    track.style.transform = `translateX(${transform + diff}px)`;
+  }
+  lastPageX = e.pageX;
+}
+
+const gestureEnd = (e) => {
+  moving = false;
+}
+
+if (window.PointerEvent) {
+  window.addEventListener('pointerdown', gestureStart);
+  window.addEventListener('pointermove', gestureMove);
+  window.addEventListener('pointerup', gestureEnd);
+} else {
+  window.addEventListener('touchstart', gestureStart);
+  window.addEventListener('touchmove', gestureMove);
+  window.addEventListener('touchend', gestureEnd);
+
+  window.addEventListener('mousedown', gestureStart);
+  window.addEventListener('mousemove', gestureMove);
+  window.addEventListener('mouseup', gestureEnd);
+}
